bccba8a5811d7e572a60d2ecccd95340a95d6678
Carlos Brito <cbrito1994@gmail.com>
2021-04-05 10:50:04 -0500

    Created the questions.js file to store the questions and related the html to both js files

---
+++ questions.js
@@ -0,0 +1,33 @@
+let QandA = [
+    {
+        question: 'Who is considered to be the "Great One" of hockey history?',
+        options: {
+            allOptions: ["Mario Lemieux", "Jaromir Jagr", "Bobby Orr", "Wayne Gretzky"],
+            correctOption: "Wayne Gretzky",
+        }
+    },{
+        question: 'Who scored the winning goal for the goldmedal game in the Vancouver 2010 Winter Olympics?',
+        options: {
+            correctOption: "Sidney Crosby",
+            allOptions: ["Ryan Kesler", "Alexander Ovechkin", "Corey Perry", "Sidney Crosby"]
+        } 
+    },{
+        question: 'Which player allias is "The Great Eight"?',
+        options: {
+            allOptions: ["Alexander Ovechkin", "Patrick Kane", "Jonathan Toews", "Dimitri Orlov"],
+            correctOption: "Alexander Ovechkin",
+        } 
+    },{
+        question: 'Who was the first overall pick in the 2015 NHL Entry Draft?',
+        options: {
+            allOptions: ["Jack Eichel", "Auston Matthews", "Connor McDavid", "Gabriel Landeskog"],
+            correctOption: "Connor McDavid",
+        } 
+    },{
+        question: 'What team was the winner of the Stanley Cup in 2020?',
+        options: {
+            allOptions: ["Dallas Stars", "Tampa Bay Lightining", "Washington Capitals", "Florida Panthers"],
+            correctOption: "Tampa Bay Lightining",
+        } 
+    }
+]
