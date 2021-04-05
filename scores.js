26c08925c426701a8b38447bca4077f455891695
Carlos Brito <cbrito1994@gmail.com>
2021-04-05 12:21:41 -0500

    Sorting the info from the local storage in descending order, creating a div for each object in the retrieved data and putting each one of them in the HTML file

--- scores.js
+++ scores.js
@@ -1,3 +1,22 @@
-let scoresContainer = document.querySelector('.container-scores');
 let scoresData = localStorage.getItem("scores");
+let scoresContainer = document.querySelector('.container-scores');
 let buttonClear = document.querySelector('.button-clear');
+
+if(scoresData !== null){
+    let scoresObj = JSON.parse(scoresData);
+    scoresObj.sort((a, b) => {
+        if(a.score > b.score){
+            return -1;
+        }
+        if(a.score < b.score){
+            return 1;
+        }
+        return 0;
+    });
+    scoresObj.forEach((scores, index) => {
+        let div = document.createElement("div");
+        div.setAttribute("class", "scoreInitials-time")
+        div.innerHTML = `${index + 1}: ${scores.initials} --> ${scores.score} points`;
+        scoresContainer.appendChild(div);
+    });
+}
